52ed3a756fb6f6dec23d89fd7a101e1e7c3ea420
Rodolfo Aguiar <rodolfo269301@gmail.com>
2026-03-22 13:35:41 -0600

    Agrega modelos MaterialAnimacion, AsignacionCoordnador, MaterialEvento y corrige bug nombre duplicado en Empleado

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
+from tabnanny import verbose
+
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -317,7 +319,6 @@ class PedidoFinanzas(models.Model):
         return f"Renta {self.renta.id} - {'Pagado' if self.pagado else 'Pendiente'}"
 
 class Empleado(models.Model):
-    nombre = models.CharField(max_length=100)
     telefono = models.CharField(max_length=20, blank=True)
     correo = models.EmailField(blank=True, null=True)
     sueldo_diario = models.DecimalField(max_digits=8, decimal_places=2)
@@ -563,3 +564,63 @@ class BitacoraMantenimiento(models.Model):
 
     def __str__(self):
         return f"{self.producto.nombre} - Mantto"
+
+class MaterialAnimacion(models.Model):
+    TIPO = [
+        ('REUTILIZABLE', 'Reutilizable'),
+        ('CONSUMIBLE', 'Consumible'),
+    ]
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    tipo = models.CharField(max_length=20, choices=TIPO, default='REUTILIZABLE')
+    stock_total = models.PositiveIntegerField(default=0)
+    foto = models.ImageField(upload_to='materiales/', blank=True, null=True)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        verbose_name = "Material Animación"
+        verbose_name_plural = "Materiales nimación"
+        ordering = ('nombre',)
+
+class AsignacionCoordinador(models.Model):
+    renta = models.OneToOneField(
+        Renta,
+        on_delete=models.CASCADE,
+        related_name='asignacion_coordinador'
+    )
+    coordinador = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='eventos_asignados'
+    )
+    notas = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.renta.folio} → {self.coordinador}"
+    class Meta:
+        verbose_name = "Asignacion Coordinador"
+
+class MaterialEvento(models.Model):
+    asignacion = models.ForeignKey(
+        AsignacionCoordinador,
+        on_delete=models.CASCADE,
+        related_name='materiales'
+    )
+    material =models.ForeignKey(
+        MaterialAnimacion,
+        on_delete=models.CASCADE,
+        related_name='usos'
+    )
+    cantidad = models.PositiveIntegerField(default=1)
+    nota = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+    def __str__(self):
+        return f"{self.material.nombre} x{self.cantidad}"
+    class Meta:
+        verbose_name = "Material del Evento"
+        unique_together = ('asignacion', 'material')
