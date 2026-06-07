173c53e6617b39c0694ff52b8be9e297f8715741
Rodolfo Aguiar <rodolfo269301@gmail.com>
2026-06-07 10:10:22 -0600

    fix: agregar directorio global de templates en DIRS

--- crm_trota/settings.py
+++ crm_trota/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'crm_trota.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
