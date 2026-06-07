9e9c3eb89643f1bfa4ffab79434f19a9a824fce6
Rodolfo Aguiar <rodolfo269301@gmail.com>
2026-06-07 13:05:51 -0600

    fix: agregar contenido a archivos vacios de core/api

---
+++ core/api/urls.py
@@ -0,0 +1,17 @@
+from rest_framework.routers import DefaultRouter
+from core.api.views import (
+    ClienteViewSet, ProductoViewSet, RentaViewSet,
+    EmpleadoViewSet, NominaViewSet, GastoViewSet,
+    MovimientoContableViewSet
+)
+
+router = DefaultRouter()
+router.register(r'clientes', ClienteViewSet)
+router.register(r'productos', ProductoViewSet)
+router.register(r'rentas', RentaViewSet)
+router.register(r'empleados', EmpleadoViewSet)
+router.register(r'nomina', NominaViewSet)
+router.register(r'gastos', GastoViewSet)
+router.register(r'movimientos', MovimientoContableViewSet)
+
+urlpatterns = router.urls
