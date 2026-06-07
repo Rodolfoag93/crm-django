b6155666c0cd64ae06e8d9d0a37a0f2ab17ec7e4
Rodolfo Aguiar <rodolfo269301@gmail.com>
2026-06-07 15:28:51 -0600

    fix: cambiar LOGIN_URL para evitar redirect en API REST

--- crm_trota/settings.py
+++ crm_trota/settings.py
@@ -149,8 +149,8 @@ STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/'
-LOGIN_REDIRECT_URL = '/home/'
+LOGIN_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # ============================
